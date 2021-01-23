3b6ae932f175427224cc0299d237c7e5a611d2c5
vukovic96 <vukovic.aleksandar96@gmail.com>
2021-01-23 20:08:16 +0100

    added view for grouped employees by region

---
+++ Projekat-2/Controllers/GroupedEmployeesByRegionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekat_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat_2.Controllers
+{
+    public class GroupedEmployeesByRegionController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public GroupedEmployeesByRegionController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = from asoc in _context.EmployeeTerritories
+                         join ter in _context.Territories on asoc.TerritoryId equals ter.TerritoryId
+                         join reg in _context.Regions on ter.RegionId equals reg.RegionId
+                         group asoc.EmployeeId by new { reg.RegionId, reg.RegionDescription } into grouped
+                         select new GroupedEmployeesByRegion(grouped.Key.RegionId, grouped.Key.RegionDescription, grouped.Count());
+            
+            return View(result.ToList());
+        }
+    }
+}
---
+++ Projekat-2/Models/GroupedEmployeesByRegion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat_2.Models
+{
+    public class GroupedEmployeesByRegion
+    {
+
+        public int RegionId { get; set; }
+        public string RegionDescription { get; set; }
+        public int employeesNumber { get; set; }
+
+        public GroupedEmployeesByRegion(int regionId, string regionDescription, int employeesNumber)
+        {
+            RegionId = regionId;
+            RegionDescription = regionDescription;
+            this.employeesNumber = employeesNumber;
+        }
+    }
+}
