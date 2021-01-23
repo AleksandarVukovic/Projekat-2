839b6edd8a968ed24f7587ee2c8537074c6cd163
vukovic96 <vukovic.aleksandar96@gmail.com>
2021-01-23 17:13:40 +0100

    added serach products part

---
+++ Projekat-2/Controllers/SearchProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projekat_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat_2.Controllers
+{
+    public class SearchProductsController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public SearchProductsController(NorthwindContext context)
+        {
+            _context = context;            
+        }
+
+        public IActionResult Index()
+        {
+            SearchProducts searchProducts = createDefaultModel();
+
+            return View(searchProducts);
+        }
+
+        private SearchProducts createDefaultModel()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            List<Supplier> suppliers = _context.Suppliers.ToList();
+
+            SearchProducts model = new SearchProducts();
+            model.suppliers = suppliers.ToList().Select(x => new SelectListItem(x.CompanyName, x.SupplierId.ToString()));
+            model.categories = categories.ToList().Select(x => new SelectListItem(x.CategoryName, x.CategoryId.ToString()));
+
+            return model;
+        }
+
+        [HttpPost]
+        public IActionResult SearchAct(SearchProducts model)
+        {
+            int categoryId = Int32.Parse(model.selectedCategoryId);
+            int supplierId = Int32.Parse(model.selectedSupplierId);
+
+            SearchProducts searchProducts = createDefaultModel();
+            searchProducts.selectedCategoryId = model.selectedCategoryId;
+            searchProducts.selectedSupplierId = model.selectedSupplierId;
+           searchProducts.products = _context.Products
+                .ToList()
+                .Where(x => x.CategoryId == categoryId && x.SupplierId == supplierId)
+                .Select(x => x);
+            return View("~/Views/SearchProducts/Index.cshtml", searchProducts);
+        }
+    }
+}
---
+++ Projekat-2/Models/SearchProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Projekat_2.Models
+{
+    public class SearchProducts
+    {
+        public SearchProducts()
+        {
+            this.products = new List<Product>();
+        }
+
+        public String selectedSupplierId { get; set; }
+        public  IEnumerable<SelectListItem> suppliers { get; set; }
+
+        public String selectedCategoryId { get; set; }
+        public IEnumerable<SelectListItem> categories{ get; set; }
+
+        public IEnumerable<Product> products { get; set; }
+
+    }
+}
